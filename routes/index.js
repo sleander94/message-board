4b81b738379d2ec15b3c7c93ef66028a4089f66e
Stephen Leander <stephen.leander1@gmail.com>
2022-06-28 00:11:47 +0200

    Render mini messageboard

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res, next) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
